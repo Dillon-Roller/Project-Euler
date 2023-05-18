d68b2a6578e33ec3f1b0a3e8eb4a38bfc8c130ea
Dillon-Roller <dillonroller.18@gmail.com>
2023-05-17 20:32:05 -0600

    updates

--- src/problem1.rs
+++ src/problem1.rs
@@ -1,15 +1,13 @@
-fn solve_problem1() -> i32 {
-    (2..1000)
-        .filter(|x| x % 3 == 0 || x % 5 == 0)
-        .sum()
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn test_problem1() {
-        assert_eq!(solve_problem1(), 233168);  // replace this with your function and expected result
+        assert_eq!(solve_problem1(), 233168);  
+    }
+
+    fn solve_problem1() -> i32 {
+        (2..1000)
+            .filter(|x| x % 3 == 0 || x % 5 == 0)
+            .sum()
     }
 }
---
+++ src/problem2.rs
@@ -0,0 +1,34 @@
+#[cfg(test)]
+mod tests {
+    pub struct Fibonacci {
+        curr: u32,
+        next: u32,
+    }
+    
+    impl Fibonacci {
+        pub fn new() -> Fibonacci {
+            Fibonacci { curr: 0, next: 1 }
+        }
+    }
+    
+    impl Iterator for Fibonacci {
+        type Item = u32;
+    
+        fn next(&mut self) -> Option<u32> {
+            let new_next = self.curr + self.next;
+            self.curr = self.next;
+            self.next = new_next;
+    
+            Some(self.curr)
+        }
+    }
+    
+    #[test]
+    fn test_problem2() {
+        assert_eq!(solve_problem2(), 233168);  // replace this with your function and expected result
+    }
+
+    fn solve_problem2() -> i32 {
+        3
+    }
+}
