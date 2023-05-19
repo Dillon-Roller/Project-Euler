1d8437b680808e370980fbf08cc60164bb8fb76d
Dillon-Roller <dillonroller.18@gmail.com>
2023-05-18 23:28:47 -0500

    trivial solution for problem4

--- src/problem4.rs
+++ src/problem4.rs
@@ -2,7 +2,17 @@
 mod tests {
     #[test]
     fn test_problem4() {
-        
+        assert!(is_palindrome(1001));
+        assert!(!is_palindrome(1002));
+
+        assert_eq!(solve_problem4(), 997799);
+    }
+
+    fn solve_problem4() -> u64 {
+        (2..999*999)
+            .filter(|&x| is_palindrome(x))
+            .last()
+            .unwrap()
     }
 
     fn is_palindrome(n: u64) -> bool {
