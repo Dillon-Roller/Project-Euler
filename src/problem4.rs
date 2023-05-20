17d2976a5b52de24ba4ae502069def7699a152fc
Dillon-Roller <dillonroller.18@gmail.com>
2023-05-19 23:30:25 -0400

    updates

--- src/problem4.rs
+++ src/problem4.rs
@@ -5,21 +5,20 @@ mod tests {
         assert!(is_palindrome(1001));
         assert!(!is_palindrome(1002));
 
-        assert_eq!(solve_problem4(), 997799);
+        assert_eq!(solve_problem4(), 906609);
     }
 
     fn solve_problem4() -> u64 {
-        (100*100..999*999)
-            .rev()
-            .find(|&n| is_palindrome(n))
+        (100..1000)
+            .flat_map(|i| {
+                (i..1000).map(move |j| i * j)
+            })
+            .filter(|&n| is_palindrome(n))
+            .max()
             .unwrap()
     }
     
     fn is_palindrome(n: u64) -> bool {
-        let s = n.to_string();
-        
-        s == s.chars()
-            .rev()
-            .collect::<String>()
+        n.to_string().chars().collect::<String>() == n.to_string().chars().rev().collect::<String>()
     }
 }
