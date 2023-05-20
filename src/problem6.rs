12e8b4b35978b6e995e66cf4c8f64e8f8e2cac24
Dillon-Roller <dillonroller.18@gmail.com>
2023-05-20 01:01:16 -0400

    p6 updates

--- src/problem6.rs
+++ src/problem6.rs
@@ -2,10 +2,15 @@
 mod tests {
     #[test]
     fn test_problem6() {
-        assert_eq!(solve_problem6(), 232792560);
+        assert_eq!(solve_problem6(), 25164150);
     }
 
     fn solve_problem6() -> u64 {
-        (1..=20).fold(1, lcm)
+        let n = 100;
+    
+        let sum_of_squares: u64 = (1..=n).map(|x| x * x).sum();
+        let square_of_sum: u64 = (1..=n).sum::<u64>().pow(2);
+    
+        square_of_sum - sum_of_squares
     }
 }
