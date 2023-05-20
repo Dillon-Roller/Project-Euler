c41906e32b16ee73b230a4c75816b0f0ff4050fe
Dillon-Roller <dillonroller.18@gmail.com>
2023-05-20 01:34:00 -0400

    updates

--- src/problem4.rs
+++ src/problem4.rs
@@ -7,18 +7,18 @@ mod tests {
     }
 
     fn solve_problem4() -> i32 {
-        (100..1000).rev()
+        (100..10000).rev()
             .map(|num| {
                 let palindrome = num.to_string() + &num.to_string().chars().rev().collect::<String>();
                 palindrome.parse::<i32>().unwrap()
             })
-            .find(|&n| can_be_factored(n))
+            .find(can_be_factored_two_3_digits)
             .unwrap()
     }
     
-    fn can_be_factored(n: i32) -> bool {
-        (100..1000)
-            .any(|i| n % i == 0 && (100..=999).contains(&(n / i)))
+    fn can_be_factored_two_3_digits(n: &i32) -> bool {
+        (100..1000).any(|i| {
+            n % i == 0 && (100..=999).contains(&(n / i))
+        })
     }
-    
 }   
